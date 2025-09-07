dfcf7e9fc982e538dd5e29bfcd2dd4ae0a504e50
Audric Osei <oseiaudric@gmail.com>
2025-09-07 13:09:20 -0400

    Fix Request.js import casing in requests.js

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
-import Request from "../models/Request.js";
+import Request from "../models/request.js";
 import Stripe from "stripe";
 
 const router = express.Router();
